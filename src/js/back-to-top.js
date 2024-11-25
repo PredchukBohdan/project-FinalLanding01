b54d4ba27d77d0d806d35674ad3f8cd3fb3eada7
Bohdan Predchuk <predchukbohdan1995@ukr.net>
2024-11-25 22:26:22 +0200

    updated

--- src/js/back-to-top.js
+++ src/js/back-to-top.js
@@ -1,5 +1,13 @@
 (() => {
   const topButton = document.getElementById('back-to-top');
+  const scroll = window.scrollY;
+
+  if (scroll > 500) {
+    topButton?.classList.add('active');
+  } else {
+    topButton?.classList.remove('active');
+  }
+
   window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
       topButton?.classList.add('active');
