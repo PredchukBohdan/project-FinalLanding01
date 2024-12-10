f8b36bdd642947418fc69e5ae644b1d1321f7c7d
Bohdan Predchuk <predchukbohdan1995@ukr.net>
2024-12-10 03:09:02 +0200

    added popup,tabs

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import '../src/js/header';
 import '../src/js/mobile-menu';
 import '../src/js/observe';
 import '../src/js/back-to-top';
+import '../src/js/popup';
+import '../src/js/reviews-slider';
+import '../src/js/tabs';
